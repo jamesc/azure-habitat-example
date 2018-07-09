1dc3230af9197e36a0b4a39be093908672d7a13d
James Casey <jamesc.000@gmail.com>
2018-07-09 20:27:33 +0000

    Final fixup for hook

--- osba-mysql-proxy/plan.sh
+++ osba-mysql-proxy/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=osba-mysql-proxy
 pkg_origin=azure-habitat-example
-pkg_version="0.1.0"
+pkg_version="0.1.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_description="A Proxy Service for MySQL service created via Open Service Broker for Azure (OSBA)"
@@ -9,7 +9,6 @@ pkg_deps=(
     core/busybox-static
     core/curl
     core/jq-static
-    core/kubectl
 )
 
 pkg_exports=(
